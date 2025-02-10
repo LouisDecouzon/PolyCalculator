82ea7ab6d060e324b109eaf45c89742b5cc731a7
LouisDecouzon <louis.decouzon.pro@gmail.com>
2025-02-10 21:50:28 +0100

    frontend

---
+++ application/back.py
@@ -0,0 +1,4 @@
+#Le back en python
+#Réceptionne la demande de calcul, la formate, calcule le résultat, 
+#l'envoie avec le calcul à la BDD puis Narnia puis
+#le back redemande le résultat et l'envoie au front
---
+++ application/index.js
@@ -0,0 +1,10 @@
+function display(val){
+    if(val == "AC"){
+        document.getElementById("result").value=""
+    }
+    else{
+        document.getElementById("result").value+=val
+    }
+    
+
+}
